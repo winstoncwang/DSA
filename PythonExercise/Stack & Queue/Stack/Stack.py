7021ee65d743298c3e1524b17f7177aa4fa28902
winstoncwang <congwang@live.co.uk>
2024-04-04 23:48:06 +0100

    Stack implementation

---
+++ PythonExercise/Stack & Queue/Stack/Stack.py
@@ -0,0 +1,41 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+        
+class Stack:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.top = new_node
+        self.length = 1
+        
+    def push(self, value):
+        new_node = Node(value)
+        new_node.next = self.top
+        self.top = new_node
+        self.length += 1
+        return True
+    
+    def pop(self):
+        if self.top is None:
+            print("Empty stack")
+            return None
+        tmp = self.top
+        self.top = tmp.next
+        tmp.next = None
+        self.length -= 1
+        return True
+        
+    def printStack(self):
+        tmp = self.top
+        while tmp is not None:
+            print(tmp.value)
+            tmp = tmp.next
+        return
+    
+    
+stack = Stack(1)
+stack.push(2)
+stack.pop()
+stack.pop()
+stack.printStack()
